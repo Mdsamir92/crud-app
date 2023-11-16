e41c56a1580e2d2c4efac90c4070788eb94b0349
Mohammad Samir <87142605+Mdsamir92@users.noreply.github.com>
2023-11-16 16:41:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import "./App.css";
 import Create from "./Create";
 import Read from "./components/Read";
 import Update from "./components/Update";
 import { BrowserRouter,Routes,Route}
 from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
 
 
 function App() {
