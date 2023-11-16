2432daa15fc10ddf8d0379cefe78f3c5e7226373
Mohammad Samir <87142605+Mdsamir92@users.noreply.github.com>
2023-11-16 16:28:28 +0530

    Update Create.js

--- src/Create.js
+++ src/Create.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import "./Create.css"
+import "./App.css";
 
 function Create() {
 
